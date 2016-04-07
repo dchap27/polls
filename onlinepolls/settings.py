3b33ff8d6d49a969e2fc4a3dc1a0b4183318d001
Ahmad Adelaja <ahmadadelaja@gmail.com>
2016-04-07 13:58:52 +0100

    settings adjusted

--- onlinepolls/settings.py
+++ onlinepolls/settings.py
@@ -134,7 +134,7 @@ DEFAULT_FROM_EMAIL = 'Naija Polls <noreply@naijapolls.com>'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gentlechap27@gmail.com'
-EMAIL_HOST_PASSWORD ='gatewayahmad'
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_TLS = True
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

95038d38b0b5bff68f0f4168c18d552d1b5aa343
Ahmad Adelaja <ahmadadelaja@gmail.com>
2016-06-17 22:58:43 +0100

    clean all

--- polls/static/polls/search.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#index-search button").click(function () {
-    var query = $("#id_query").val();
-    $('.search-result').load(
-      "polls/search/?ajax&query=" + encodeURIComponent(query));
-      return false;
-  });
-});
